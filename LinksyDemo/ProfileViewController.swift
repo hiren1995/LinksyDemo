36956c03a5f82a9fcb0713cc01953c8906788fcd
hiren1995 <kadamhiren.sigma@gmail.com>
2017-10-04 15:13:11 +0530

    alert for Error 404 added

--- LinksyDemo/ProfileViewController.swift
+++ LinksyDemo/ProfileViewController.swift
@@ -598,6 +598,12 @@ class ProfileViewController: UIViewController,BEMCheckBoxDelegate,UICollectionVi
                 else
                 {
                     print("Error")
+                    
+                    let alert = UIAlertController(title: "Error 404", message: "Please check your network Connection and try again", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    
+                    self.present(alert, animated: true, completion: nil)
                 }
                 
             }
