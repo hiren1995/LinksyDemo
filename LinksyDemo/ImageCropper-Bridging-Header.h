a493c1444ce753def5891c2f90019db3da9c9614
hiren1995 <kadamhiren.sigma@gmail.com>
2017-10-28 18:12:46 +0530

    bridging header image cropper

---
+++ LinksyDemo/ImageCropper-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ImageCropper-Bridging-Header.h
+//  LinksyDemo
+//
+//  Created by APPLE MAC MINI on 28/10/17.
+//  Copyright © 2017 APPLE MAC MINI. All rights reserved.
+//
+
+#ifndef ImageCropper_Bridging_Header_h
+#define ImageCropper_Bridging_Header_h
+
+
+#endif /* ImageCropper_Bridging_Header_h */
